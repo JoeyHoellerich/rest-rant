304f6941c3cc6f5311c608fc7df0ca706b7b39e1
Joey <joeyhoellerich@gmail.com>
2022-05-23 20:30:56 -0400

    comments

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// require .env file package (allows us to include a .env file) - holds PORT
 require("dotenv").config();
+
+// require the express package
 const express = require("express");
+// instantiate express package
 const app = express();
 
+// get request for homepage
 app.get("/", (req, res) => {
     res.send("Hello my Guy!");
 })
 
+// post request for homepage (TEST)
 app.post("/", (req, res) => {
     res.send("This is a Post!")
 })
 
+// error 404 page (page not found)
 app.get("*", (req, res) => {
-    res.send("Happy Error 404");
+    res.status(404).send("Happy Error 404");
 })
 
+// start web server
 app.listen(process.env.PORT);
