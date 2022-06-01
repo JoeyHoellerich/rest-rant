6e8d27dcd4481c8a4d56d58576b3230d0366c1fa
Joey <joeyhoellerich@gmail.com>
2022-06-01 17:18:00 -0400

    added defualt view

---
+++ views/default.jsx
@@ -0,0 +1,20 @@
+// require React 
+const React = require("react");
+
+// define default function for creating html wrapper
+function Def (html) {
+    // function will return html format below
+    <html>
+        <head>
+            <title>Title</title>
+        </head>
+        <body>
+            {/* any children of the passed in html will go here */}
+            {html.children}
+        </body>
+    </html>
+
+}
+
+// export default function
+module.exports = Def
