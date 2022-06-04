4c51941d146824a08775398b57678e2a831903b3
Joey <joeyhoellerich@gmail.com>
2022-06-04 09:57:07 -0400

    added style to default page

--- views/default.jsx
+++ views/default.jsx
@@ -14,6 +14,19 @@ function Def (html) {
                 <link rel="stylesheet" href= "/css/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {/* any children of the passed in html will go here */}
                 {html.children}
             </body>
